3473646d42e8ab142963936b9edfb8e818ff5a56
D-Kunrath <dkunrath@gmail.com>
2020-10-09 17:29:18 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Configura o app para entender requisições com tipo de corpo JSON
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
+
+const projectRouter = require("./routes/project.routes");
+const taskRouter = require("./routes/task.routes");
+const authRouter = require("./routes/auth.routes");
+
+require("./configs/db.config");
+
+require("./configs/passport.config")(app);
+
+// app.use("/api", projectRouter);
+// app.use("/api", taskRouter);
+// app.use("/api", authRouter);
+
+app.listen(4000, () => console.log("running at port 4000"));
