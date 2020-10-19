7a06dcfeaf699fb62d231f7a12218ec79f9f51db
Rubem Diogo <rubemdiogo@gmail.com>
2020-10-19 13:02:50 -0400

    mongo atlas

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,13 +1,31 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost/project-class-repo-server", {
+const MONGODB_URI = process.env.MONGODB_URI; //|| "mongodb:localhost/project-class-repo-server";
+
+module.exports = mongoose
+  .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .then((x) =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
-  .catch((err) => console.error("Error connecting to mongo", err));
+  .then(() => console.log(`Successfully connected to the database`))
+  .catch((error) => {
+    console.error(
+      `An error ocurred trying to connect to the database  `,
+      error
+    );
+    process.exit(1);
+  });
+
+// mongoose
+//   .connect("mongodb://localhost/project-class-repo-server", {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   })
+//   .then((x) =>
+//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+//   )
+//   .catch((err) => console.error("Error connecting to mongo", err));
--- models/Classroom.model.js
+++ models/Classroom.model.js
@@ -1,14 +1,14 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const ClassroomSchema = new Schema(
   {
-    user_id: { type: Schema.Types.ObjectId, ref: 'User' },
+    user_id: { type: Schema.Types.ObjectId, ref: "User" },
     classroom_name: String,
     description: String,
     // repo_ids: [{ type: Schema.Types.ObjectId, ref: 'Repo' }],
-    students: [ String ]
+    students: [String],
   },
   { timestamp: true }
-)
+);
 
-module.exports = model('Classroom', ClassroomSchema);
+module.exports = model("Classroom", ClassroomSchema);
